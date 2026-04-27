7877014af5b4686e52fc0aae475763039e11a6e0
Eli Newsham <newshameli@gmail.com>
2026-04-27 14:49:15 -0400

    more fixes

--- Assets/_Scripts/SettingsHighlight.cs
+++ Assets/_Scripts/SettingsHighlight.cs
@@ -5,8 +5,13 @@ public class SettingsHighlight : MonoBehaviour
 {
   public Image bg; //buttonBG
 
-  Color regColor = new Color(0.729f, 0.455f, 0.090f, 0.22f);
+  Color regColor;
   Color hoverColor = new Color(0.729f, 0.455f, 0.090f, 0.55f);
+
+  void Start()
+  {
+    regColor = bg.color;
+  }
 
   public void OnHoverEnter()
   {
